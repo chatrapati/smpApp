2050339737eb37eab5ec0d0f3f79cd216bb78aee
Gayathri <gayathritummapala@gmail.com>
2018-02-21 15:27:44 +0530

    Gayathri
    conflicts

--- www/js/constants.js
+++ www/js/constants.js
@@ -19,5 +19,7 @@ angular.module('shopMyTools.constants', [])
 .constant('LOGIN_URL','http://157.119.108.135:8005')
 
 .constant('PRODUCT_DETAIL_SERVICE', 'http://157.119.108.139:8005')
+//.constant('PRODUCT_DETAIL_SERVICE', 'http://192.168.20.66:8000')
+
 
 .constant('PRODUCT_CATEGORY_SERVICE','http://157.119.108.137:80'); 
