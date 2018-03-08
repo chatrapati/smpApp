f85d84d7c555b11acb9c9a2e6b3f9120a8e4a981
Gayathri <gayathritummapala@gmail.com>
2018-03-08 18:59:39 +0530

    Gayathri
    Reviews and ratings

--- www/js/constants.js
+++ www/js/constants.js
@@ -4,7 +4,7 @@ angular.module('shopMyTools.constants', [])
 
    //  .constant('LOGIN_URL','http://192.168.20.69:8080')
 
-    //.constant('SERVER_URL1','http://192.168.20.65:8090')
+    .constant('SERVER_URL1','http://192.168.20.65:8000')
 
     //.constant('PRODUCT_CATEGORY_SERVICE', 'http://192.168.20.67:8000')
 
@@ -19,7 +19,7 @@ angular.module('shopMyTools.constants', [])
 
     .constant('SERVER_URL', 'http://157.119.108.141:80')
 
-    .constant('SERVER_URL1', 'http://157.119.108.142:8002')
+    //.constant('SERVER_URL1', 'http://157.119.108.142:8002')
 
     .constant('PRODUCT_DETAIL_SERVICE', 'http://157.119.108.139:8005')
     
